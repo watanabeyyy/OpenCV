3aaf7774039f2da8f29a75744f498e6998bfeea9
watanabeyyy <yuki.watanabe@furukawaelectric.com>
2017-10-02 09:40:39 +0900

    fix algo

--- scripts/MD_cnt.py
+++ scripts/MD_cnt.py
@@ -2,99 +2,77 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 def trim(img):
     img_x = np.shape(img)[1]
     for i in range(img_x):
         if (img[100, i] > 10):
-            trim_l = i - 10
+            trim_l = i - 3
             break
     for i in range(img_x):
         if (img[100, img_x - (i + 1)] > 10):
-            trim_r = img_x - (i + 1) + 10
+            trim_r = img_x - (i + 1) + 2
             break
     img = img[:, trim_l:trim_r]
 
     return img
 
-def hist_bin(img):
-    #img = cv2.equalizeHist(img)
-    #binarize = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 501, 50)
 
-    img_flatten = img.flatten()
-
-    # 各ビンを [0, 1), [1, 2), ..., [255, 256] とすることで、各輝度値の頻度を計算する。
-    # np.histogram() のリファレンス参照。
-    bins = np.arange(0, 257)
-    hist, bin_edges = np.histogram(img_flatten, bins)
-    bin_edges = bin_edges[:-1]
-
-    # 頻度値の積算を計算する
-    cumsum = np.cumsum(hist)
-
-    # 対象物体が画像に占める割合が 50 %であるとわかっている場合
-    p = 0.1
-    for i in range(0, 256):
-        pcent = cumsum[i] / cumsum[-1]
-        if pcent > p:
-            break
-
-    _, binarize = cv2.threshold(img, i, 255, cv2.THRESH_BINARY_INV)
-
-    maisu = 0
-
-    for i in range(np.shape(binarize)[1]):
-        num = len(np.where(binarize[:,i] == 255)[0])
-        if (num > 0.3 * np.shape(binarize)[0]):
-            binarize[:,i] = 255
-
+def make_bin(img, th_bri=50, th_num=0.4):
+    binarize = np.zeros(np.shape(img)[1])
+    c_num = np.shape(img)[1]
+    for i in range(c_num):
+        sub_im = img[:, i]
+        num = len(np.where(sub_im < th_bri)[0])
+        if (num > th_num * np.shape(img)[0]):
+            binarize[i] = 0
+        else:
+            binarize[i] = 255
     return binarize
 
+
 def cnt_disk(sub_bin, sub_bin_y):
-    label = cv2.connectedComponentsWithStats(sub_bin)
-    n = label[0] - 1
-    data = np.delete(label[2], 0, 0)
-    center = np.delete(label[3], 0, 0)
-    # print(u"ブロブの個数:", n)
-    # print(u"各ブロブの外接矩形の左上x座標", data[:, 0])
-    # print(u"各ブロブの外接矩形の左上y座標", data[:, 1])
-    # print(u"各ブロブの外接矩形の幅", data[:, 2])
-    # print(u"各ブロブの外接矩形の高さ", data[:, 3])
-    # print(u"各ブロブの面積", data[:, 4])
-    #print(u"各ブロブの中心座標:\n", center)
-    num = len(np.where(data[:, 3] > sub_bin_y - 10)[0])
-    #print(u"谷の中心座標:\n", center[np.where(data[:, 3] > sub_bin_y - 10)])
-    # print("枚数:", num-1)
+    black = False
+    pos = []
+    for i in range(sub_bin_y):
+        if (i == 0):
+            if (sub_bin[i] == 0):
+                black = True
+                str = i
+            else:
+                black = False
+        elif (i == sub_bin_y - 1):
+            if (black):
+                end = i - 1
+                pos.append((str, end))
+        else:
+            if (not (black) and sub_bin[i] == 0):
+                black = True
+                str = i
+            elif (black and sub_bin[i] == 255):
+                black = False
+                end = i - 1
+                pos.append((str, end))
+    num = len(pos)
 
-    return num , center[np.where(data[:, 3] > sub_bin_y - 10)][:,0]
+    point = np.zeros(num)
 
+    for i in range(num):
+        point[i] = round((pos[i][0] + pos[i][1]) / 2)
 
-# img_x = np.shape(img)[1]
-# img_y = np.shape(img)[0]
-# sub_bin_y = 128
-#
-# num_sub_bin = int(img_y / sub_bin_y)
-# num_sub_bin = 10
-# pre_cnt = np.zeros(num_sub_bin)
-#
-# binarize = hist_bin(img)
-#
-# for i in range(num_sub_bin):
-#     sub_bin = binarize[i:(i + 1) * sub_bin_y - 1, :]
-#     pre_cnt[i] = cnt_disk((sub_bin), sub_bin_y)
-#
-# print(pre_cnt)
-# print(stats.mode(pre_cnt))
+    return num, point
+
 
 if __name__ == "__main__":
     org_img = cv2.imread('../img/56_6.bmp', cv2.IMREAD_GRAYSCALE)
     img = trim(org_img)
-    binarize = hist_bin(img)
-    num , point = cnt_disk(binarize, np.shape(binarize)[0])
+    bin = make_bin(img)
+    num, point = cnt_disk(bin, np.shape(bin)[0])
 
-    print("谷位置:",point)
-    print(num-1,"枚")
+    print("谷位置:", point)
+    print(num - 1, "枚")
 
-    size = 3
+    size = 2
 
     plt.subplot(1, size, 1)
     plt.imshow(org_img)
@@ -104,11 +82,6 @@ if __name__ == "__main__":
     plt.subplot(1, size, 2)
     plt.imshow(img)
     plt.title("trim->hist_equalize")
-    plt.xticks([]), plt.yticks([])
-
-    plt.subplot(1, size, 3)
-    plt.imshow(binarize)
-    plt.title("binalize")
     plt.xticks([]), plt.yticks([])
 
     plt.show()
--- scripts/MD_detection.py
+++ scripts/MD_detection.py
@@ -3,12 +3,62 @@ import numpy as np
 from matplotlib import pyplot as plt
 import MD_cnt
 
-org_img = cv2.imread('../img/56_6.bmp', cv2.IMREAD_GRAYSCALE)
-img = MD_cnt.trim(org_img)
-binarize = MD_cnt.hist_bin(img)
-num, point = MD_cnt.cnt_disk(binarize, np.shape(binarize)[0])
-print(num-1,"枚")
 
+def match(img, temp, num, point):
+    match_rate = np.zeros(num - 1)
+    for j in range(num - 1):
+        left = int(point[j])
+        right = int(point[j + 1]) + 1
+        p = img[:, left:right]
+        p = p - np.mean(p)
+        temp_resized = cv2.resize(temp, np.shape(p)[::-1])
+        temp_resized = temp_resized - np.mean(temp_resized)
+        norm = np.linalg.norm(temp_resized) * np.linalg.norm(p)
+        zncc = np.sum(temp_resized * p)
+        if norm == 0:
+            zncc = 0
+        else:
+            zncc = zncc / norm
+        match_rate[j] = zncc
+    return match_rate
+
+
+def check_orientation(img, num, point):
+    left = int(point[0])
+    right = int(point[1]) + 1
+    temp_a = img[:, left:right]
+    temp_b = temp_a[:, ::-1]
+
+    rate_a = match(img, temp_a, num, point)
+    rate_b = match(img, temp_b, num, point)
+    ori = np.zeros(num - 1)
+    bool_a = True
+    if (rate_a[0] > rate_b[0]):
+        bool_a = True
+    else:
+        bool_a = False
+    for i in range(num - 1):
+        if (rate_a[i] >= rate_b[i] and bool_a == True):
+            ori[i] = 0
+        elif (rate_a[i] <= rate_b[i] and bool_a == False):
+            ori[i] = 0
+        else:
+            ori[i] = 1
+        bool_a = not (bool_a)
+    print(ori)
+
+
+org_img = cv2.imread('../img/4_5.bmp', cv2.IMREAD_GRAYSCALE)
+# トリミング⇒枚数カウント
+img = MD_cnt.trim(org_img)
+bin = MD_cnt.make_bin(img, th_bri=60, th_num=0.8)
+num, point = MD_cnt.cnt_disk(bin, np.shape(bin)[0])
+print(num - 1, "枚")
+
+# 方向チェック
+check_orientation(img, num, point)
+
+# 欠陥抽出
 im_x = np.shape(img)[1]
 im_y = 4096
 
@@ -18,9 +68,9 @@ num_sub_y = int(im_y / sub_im_y)
 
 matching = np.zeros((im_y, im_x))
 
-for j in range(num-1):
+for j in range(num - 1):
     left = int(point[j])
-    right = int(point[j+1])+1
+    right = int(point[j + 1]) + 1
     for i in range(num_sub_y):
         p1 = img[(i * sub_im_y):((i + 1) * sub_im_y), left:right]
         if i == 0:
@@ -29,6 +79,8 @@ for j in range(num-1):
             p0 = img[((i - 1) * sub_im_y):(i * sub_im_y), left:right]
         p0 = p0 - np.mean(p0)
         p1 = p1 - np.mean(p1)
+        if (np.shape(p0) != np.shape(p1)):
+            break
         norm = np.linalg.norm(p0) * np.linalg.norm(p1)
         zncc = np.sum(p0 * p1)
         if norm == 0:
@@ -39,6 +91,7 @@ for j in range(num-1):
 
 ret, binalize = cv2.threshold(matching, 200, 255, cv2.THRESH_BINARY)
 
+# 可視化
 size = 3
 
 plt.subplot(1, size, 1)
