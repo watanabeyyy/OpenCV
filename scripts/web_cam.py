5c933a8450fca6cf3c685e4be16da6a53b64fb81
watanabeyyy <yuki.watanabe@furukawaelectric.com>
2017-10-31 21:31:57 +0900

    update

--- scripts/web_cam.py
+++ scripts/web_cam.py
@@ -1,14 +1,18 @@
 import cv2
+import pyocr.builders
+from PIL import Image
+import numpy as np
+
+tools = pyocr.get_available_tools()
+tool = tools[0]
 
 capture = cv2.VideoCapture(1)
-capture.set(3, 640)  # Width
-capture.set(4, 480)  # Heigh
+capture.set(3, 128)  # Width
+capture.set(4, 64)  # Heigh
 capture.set(5, 15)   # FPS
 
-if capture.isOpened() is False:
-    raise("IO Error")
-
 cv2.namedWindow("Capture", cv2.WINDOW_AUTOSIZE)
+ret, image = capture.read()
 
 while True:
 
@@ -19,8 +23,11 @@ while True:
 
     cv2.imshow("Capture", image)
 
+    image = Image.fromarray(np.uint8(image))
+    res = tool.image_to_string(image)
+    print(res)
+
     if cv2.waitKey(33) >= 0:
-        cv2.imwrite("image.png", image)
         break
 
 capture.release()
